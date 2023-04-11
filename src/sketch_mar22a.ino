7e1a3dbd8fc28b07b95e1db3e99c3e34158231fa
Markus Eckl <nbhcrew@gmail.com>
2023-04-11 23:31:53 +0200

    Dev. Release
    Added option to use apds9960 sensor
    added wifi scan results to html page.
    tolerance slider now with value shown.
    more options for MQTT (Not fully implemented)
    now real sensor values on the dashboard.
    disabled background color adaptation for bandwidth savings.
    fixed some miner issues.

--- src/sketch_mar22a.ino
+++ src/sketch_mar22a.ino
@@ -8,6 +8,7 @@
 #include <AsyncElegantOTA.h>
 #include <espMqttClientAsync.h>
 #include <Adafruit_TCS34725.h>
+#include <SparkFun_APDS9960.h>
 
 #include "Index.html"   // Startpage
 #include "About.html"   // Über-Seite
@@ -23,13 +24,16 @@
 const bool WifiRestore = false;
 const char* WIFISSID = "WLAN";
 const char* WIFIPASS = "PASSWORD";
+int numberOfNetworks = 0;
 
 String LastRGB = "255,255,255"; // 255,... ist weiß und Fehler beim Sensor.
 int LastStat = 0;
 ulong StartMs = 0;
-float ConsumL = 0;
+double ConsumL = 0;
+ulong AllXSec = millis();
 ulong LastMS = 0;   // Backup-Strategie für Werte senden.
 bool APMode = true; // Wenn AP-Mode dann MQTT ignorieren
+bool TCS = true;   // Wenn TCS34725 verwendet wurde, ansonsten APDS9960
 
 bool SensReady = false; // Wenn Sensor init OK
 bool MqttReady = false; // Wenn MQTT init OK
@@ -39,29 +43,34 @@ const char* WIFIFILENAME = "Wifi.json";
 const char* MQTTFILENAME = "Mqtt.json";
 const char* SENSFILENAME = "Sens.json";
 const char* BURNFILENAME = "Burn.json";
+const char* USAGEFILENAME = "Usage.json";
 DynamicJsonDocument WIFISETT(128); // Muss größer sein als tatsächlicher Verbrauch (Sonst werden werte mit null geschrieben)
 DynamicJsonDocument SENSSETT(128); // Muss größer sein als tatsächlicher Verbrauch (Sonst werden werte mit null geschrieben)
 DynamicJsonDocument MQTTSETT(256); // Muss größer sein als tatsächlicher Verbrauch (Sonst werden werte mit null geschrieben)
 DynamicJsonDocument BURNSETT(1024);// Muss größer sein als tatsächlicher Verbrauch (Sonst werden werte mit null geschrieben)
+DynamicJsonDocument USAGE(512);    // Muss größer sein als tatsächlicher Verbrauch (Sonst werden werte mit null geschrieben)
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+SparkFun_APDS9960 apds = SparkFun_APDS9960 ();
 espMqttClientAsync mqttClient;
 AsyncEventSource events("/events"); 
 AsyncWebServer server(80);
 
 void setup(void) {
-  Serial.begin(115200);
-  Serial.println("");Serial.println(""); // Leerzeilen um vom Bootcode abzusetzen
-  pinMode (pinLED, OUTPUT);
+  Serial.begin(115200);                   // Serielle Konsole aktivieren (115200)
+  Serial.println("");Serial.println("");  // Leerzeilen um vom Bootcode abzusetzen
+  pinMode (pinLED, OUTPUT);               // LED-Pin des Sensors.
 
-  InitLittleFS (); 
+  InitLittleFS ();                        // Im LittleFS stehen die WLAN- und andere daten.
+  numberOfNetworks = WiFi.scanNetworks(); // Bevor zu einem WLAN verbunden wird, sonst Absturz!
 
-  bool LoadedWifi = LoadFile (WIFIFILENAME, WIFISETT); if(WifiRestore == true){DefaultJsonWifi(); SaveFile(WIFIFILENAME, WIFISETT);};
-  bool LoadedMqtt = LoadFile (MQTTFILENAME, MQTTSETT); if(LoadedMqtt == false){DefaultJsonMqtt(); SaveFile(MQTTFILENAME, MQTTSETT);};
-  bool LoadedSens = LoadFile (SENSFILENAME, SENSSETT); if(LoadedSens == false){DefaultJsonSens(); SaveFile(SENSFILENAME, SENSSETT);};
-  bool LoadedBurn = LoadFile (BURNFILENAME, BURNSETT); if(LoadedBurn == false){DefaultJsonBurn(); SaveFile(BURNFILENAME, BURNSETT);};
+  bool LoadedWifi = LoadFile (WIFIFILENAME, WIFISETT); if(WifiRestore == true) {DefaultJsonWifi();  SaveFile(WIFIFILENAME, WIFISETT);};
+  bool LoadedMqtt = LoadFile (MQTTFILENAME, MQTTSETT); if(LoadedMqtt == false) {DefaultJsonMqtt();  SaveFile(MQTTFILENAME, MQTTSETT);};
+  bool LoadedSens = LoadFile (SENSFILENAME, SENSSETT); if(LoadedSens == false) {DefaultJsonSens();  SaveFile(SENSFILENAME, SENSSETT);};
+  bool LoadedBurn = LoadFile (BURNFILENAME, BURNSETT); if(LoadedBurn == false) {DefaultJsonBurn();  SaveFile(BURNFILENAME, BURNSETT);};
+  bool LoadedUsage = LoadFile (USAGEFILENAME, USAGE);  if(LoadedUsage == false){DefaultJsonUsage(); SaveFile(USAGEFILENAME, USAGE);};
 
-  if (LoadedWifi){  // Wenn Wifi.json vorhanden
+  if (LoadedWifi){    // Wenn Wifi.json vorhanden
     Serial.println    ("Connect to saved AP)");
     bool connected = InitWifiSTA();    // Verbinde zu Router
     if (connected == true){
@@ -71,16 +80,17 @@ void setup(void) {
       InitWifiAP ();    // Eigener Accesspoint
       APMode = true;
     }
-  } else {            // sonst
+  } else {            // sonst Standard-AP Mode
     Serial.println    ("Emulate an AP ...");
     InitWifiAP ();    // Eigener Accesspoint
     APMode = true;
   }
 
 // MQTT nur im Client-Modus erlaubt!.
-  if (APMode == false){InitMQTT();}
-  // InitTCS34725();   // Sensor Initialisieren.
-
+  if (APMode == false && MQTTSETT["ENAB"] == "True"){InitMQTT();}         // MQTT-Initialisieren (Bool ist String aus Webinterface!)
+  if (SENSSETT["TYPE"] == "TCS34725") {InitTCS34725();}                   // entweder TCS-Sensor Initialisieren (Standard)
+  if (SENSSETT["TYPE"] == "APDS9960") {InitGYP9960 ();}                   // oder ... GYP-Sensor Initialisieren
+ 
   // HTML-Inhalte (Flash)
   server.on("/",            HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", IndexHTML); });
   server.on("/index.html",  HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", IndexHTML); });
@@ -90,14 +100,26 @@ void setup(void) {
   server.on("/Default.html",HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", DefaultHTML); });
   server.on("/style.css",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", StyleCSS); });
   server.on("/about.html",  HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", AboutHTML); });
-  server.on("/Wifi.html",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", WifiHTML); });
-  
+  server.on("/Wifi.html",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send_P(200, "text/html", WifiHTML);  });
+
+
   server.on("/Burn.json",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send(LittleFS, BURNFILENAME, "text/html");});
   server.on("/Wifi.json",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send(LittleFS, WIFIFILENAME, "text/html");});
   server.on("/Mqtt.json",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send(LittleFS, MQTTFILENAME, "text/html");});
   server.on("/Sens.json",   HTTP_GET,   [](AsyncWebServerRequest *request){ request->send(LittleFS, SENSFILENAME, "text/html");});
+  server.on("/Usage.json",  HTTP_GET,   [](AsyncWebServerRequest *request){ request->send(LittleFS, USAGEFILENAME,"text/html");});
 
-  // Parameter aus dem Webinterface
+  // Wifi-Netzwerke aus dem Start-Scan
+  server.on("/NETWORKS",    HTTP_GET,   [](AsyncWebServerRequest *request){
+    DynamicJsonDocument WifiNetworks(512); char json_string[256];
+    for(int i =0; i<numberOfNetworks; i++){
+      WifiNetworks[i]["SSID"] = WiFi.SSID(i);
+      WifiNetworks[i]["RSSI"] = WiFi.RSSI(i);
+    };
+    serializeJson(WifiNetworks, json_string);
+    request->send(200, "text/html",json_string);
+  });
+  // Parameter aus dem Webinterface    
   server.on("/SETTINGS",     HTTP_GET,   [](AsyncWebServerRequest *request){
     int paramsNr = request->params();
     Serial.println(paramsNr + "Received");
@@ -116,11 +138,15 @@ void setup(void) {
         if (p -> name() == "INTEG"){SENSSETT["INTEG"] = p-> value(); SensUpdated = true;}
         if (p -> name() == "GAIN") {SENSSETT["GAIN"]  = p-> value(); SensUpdated = true;}
         if (p -> name() == "LIGHT"){SENSSETT["LIGHT"] = p-> value(); SensUpdated = true;}
+        if (p -> name() == "TYPE") {SENSSETT["TYPE"]  = p-> value(); SensUpdated = true;}
         
         if (p -> name() == "HOST") {MQTTSETT["HOST"]  = p-> value(); MqttUpdated = true;}
         if (p -> name() == "USER") {MQTTSETT["USER"]  = p-> value(); MqttUpdated = true;}
-        if (p -> name() == "PASW") {MQTTSETT["PASS"]  = p-> value(); MqttUpdated = true;}
+        if (p -> name() == "PASW") {MQTTSETT["PASW"]  = p-> value(); MqttUpdated = true;}
         if (p -> name() == "HASS") {MQTTSETT["HASS"]  = p-> value(); MqttUpdated = true;}
+        if (p -> name() == "PORT") {MQTTSETT["PORT"]  = p-> value(); MqttUpdated = true;}
+        if (p -> name() == "TOPC") {MQTTSETT["TOPC"]  = p-> value(); MqttUpdated = true;}
+        if (p -> name() == "ENAB") {MQTTSETT["ENAB"]  = p-> value(); MqttUpdated = true;}
         
         if (p -> name() == "TOL")     {BURNSETT["TOL"]  = p-> value(); BurnUpdated = true;}
         if (p -> name() == "MAX")     {BURNSETT["MAX"]  = p-> value(); BurnUpdated = true;}
@@ -158,6 +184,7 @@ void setup(void) {
     LittleFS.remove(BURNFILENAME);
     request->send(200, "text/plain", "Sensor and Burn setting resetted.");
   });
+
   AsyncElegantOTA.begin(&server);    // Start ElegantOTA
   server.addHandler(&events);
   server.begin();
@@ -208,21 +235,35 @@ void onMqttConnect(bool sessionPresent) {
 }
 
 void loop(void) {
- 
-// Forcing alle 2 Sekunden für das Webinterface 
-// und Entlastung von MQTT bei gleichbleibenden Werten.
-bool ForcedSend = false;
-if (millis() - LastMS >= 2000) {
-  ForcedSend = true;
-  LastMS = millis ();
-}
+
+// ----------------------------------------------------------
+// Forcing der Werte alle 2 Sekunden für das Webinterface 
+// dadurch Entlastung von MQTT bei gleichbleibenden Werten.
+// ----------------------------------------------------------
+bool ForcedSend = false; if (millis() - LastMS >= 2000) {ForcedSend = true; LastMS = millis ();}
 
 if (SensReady == true){
-  // Get values and convert to int / RGB-String
-  float r, g, b,  l;
-  tcs.getRGB (&r, &g, &b);
-  l = tcs.calculateLux(r, g, b);
-  int R = (int)r;  int G = (int)g;  int B = (int)b;
+
+  // ----------------------------------------------------------
+  // Sensorwerte (TCS,GYP) erfassen und in 
+  // R,G,B,L als integer (0-255) normalisieren.
+  // ----------------------------------------------------------
+  int   R, G, B, L; // Farben (0-255), INT
+  if (TCS){
+    float r, g, b, l;
+    tcs.getRGB (&r, &g, &b);
+    l = tcs.calculateLux(r, g, b);
+    R = (int)r;  G = (int)g; B = (int)b; L=(int)l;
+  } else {
+    uint16_t r = 0, g = 0, b = 0, l = 0; 
+    if(!apds.readAmbientLight(l) ||
+      !apds.readRedLight(r) ||
+      !apds.readGreenLight(g) ||
+      !apds.readBlueLight(b)
+  ){Serial.println("Error reading light values");}
+    R = (int)r ;  G = (int)g ; B = (int)b ; L=(int)l;
+  }
+
   String RGB = String(R);RGB.concat(",");RGB.concat(G);RGB.concat(",");RGB.concat(B);
 
   if (RGB != LastRGB || ForcedSend){
@@ -230,35 +271,44 @@ if (SensReady == true){
     mqttClient.publish("oilmeter/sensor/0/light_r", 0, true, String(R).c_str());
     mqttClient.publish("oilmeter/sensor/0/light_g", 0, true, String(G).c_str());
     mqttClient.publish("oilmeter/sensor/0/light_b", 0, true, String(B).c_str());
-    mqttClient.publish("oilmeter/sensor/0/light_l", 0, true, String(l).c_str());
-    mqttClient.loop(); delay(50);yield();
+    mqttClient.publish("oilmeter/sensor/0/light_l", 0, true, String(L).c_str());
   }
 
   int S = BrennerStatus(R,G,B);
-  Serial.print("Status(ermittelt):");
-  Serial.println(S);
+  Serial.print("Status:"); Serial.println(S);
 
-  if (S == 3 && LastStat != 3){StartMs = millis();}  // Start des Brennvorgangs
+  if (S == 3 && LastStat != 3){
+    StartMs = millis();                                                                               // Start des Brenntimers
+    events.send(String(((double)millis() - (double)AllXSec) / 1000.0).c_str(), "LastWTime");          // Ausgabe der Sekunden Zwischen den Brennvorgängen
+    AllXSec = millis();                                                                                      // Zurücksetzen des Brennabstandtimers
+  }  
   if (S != 3 && LastStat == 3){
-    ulong BurnTime = (millis() - StartMs) / 1000;
-    Serial.print ("Gebrannt: "); 
-    Serial.print (BurnTime);
-    Serial.println ("Sekunden");
-    events.send(String(BurnTime).c_str(), "Burned"); // Brennzeit Sekunden
-    ConsumL += (BurnTime / 60 / 60) * BURNSETT["L_H"].as<float>() * BURNSETT["COR"].as<float>();
+    double BurnSeconds = ((double)millis() - (double)StartMs) / 1000.0;
+    double BurnHours   = BurnSeconds / 60.0 / 60.0;
+    double BurnLiter   = BurnHours * BURNSETT["L_H"].as<double>() * BURNSETT["COR"].as<double>();
+
+    Serial.println ("--------------------------------------------------------------");
+    Serial.print ("Burn_s: "); Serial.print (BurnSeconds,6); Serial.println (" seconds");
+    Serial.print ("Burn_h: "); Serial.print (BurnHours,6);   Serial.println (" hours");
+    Serial.print ("Burn_l: "); Serial.print (BurnLiter,6);   Serial.println (" Liter");
+    events.send(String(BurnSeconds,6).c_str(), "LastBTime"); 
+    events.send(String(BurnLiter,6).c_str(),   "LastConsL"); 
+    Serial.println ("--------------------------------------------------------------");
+    ConsumL += BurnLiter;
   }
 
   if (S != LastStat || ForcedSend){
     events.send(String(S).c_str(), "Status");
-    events.send(String(BURNSETT["L_H"].as<float>()).c_str(), "L_H");
-    events.send(String(BURNSETT["COR"].as<float>()).c_str(), "COR");
-    events.send(String(246 / 60 / 60).c_str(), "COR");
-    events.send(String(ConsumL).c_str(), "ConsumeL");
-    mqttClient.publish("oilmeter/Consumption_l", 0, true, String(ConsumL).c_str());
+    // events.send(String(BURNSETT["L_H"].as<float>()).c_str(), "L_H");
+    // events.send(String(BURNSETT["COR"].as<float>()).c_str(), "COR");
+    // events.send(String(246 / 60 / 60 * BURNSETT["L_H"].as<float>()).c_str(), "CALC");
+    // events.send(String(ConsumL).c_str(), "ConsumeL");
+    mqttClient.publish("oilmeter/Consumption_l", 0, true, String(ConsumL,6).c_str());
   }
 
   LastRGB = RGB;  // Set last Sensor Status
   LastStat = S;   // Set last Status
+  delay (200); yield();
 } else {
   if (ForcedSend){
     events.send("Sensor Error", "RGB");  
@@ -345,31 +395,48 @@ void InitWifiAP (){
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-}
+}     
+
 void InitMQTT(){
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-
-  mqttClient.setServer(MQTTSETT["HOST"], MQTTSETT["PASS"]);
-  mqttClient.setCredentials(MQTTSETT["USER"], MQTTSETT["PASS"]).setClientId("OilMeter");
-  Serial.println("Connecting to MQTT...");
+  IPAddress ADDR; if (ADDR.fromString(MQTTSETT["HOST"].as<const char*>())) {Serial.println ("Error resolving IP from String");}
+  mqttClient.setCredentials(MQTTSETT["USER"].as<const char*>(), MQTTSETT["PASS"].as<const char*>()).setClientId("OilMeter");
+  mqttClient.setServer(ADDR, 1883); Serial.println("Connecting to MQTT...");
   mqttClient.connect();
 }
+void InitGYP9960 (){
+  if (apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  if (apds.enableLightSensor(false) ) {
+    Serial.println(F("Light sensor is now running"));
+    SensReady = true; 
+  } else {
+    Serial.println(F("Something went wrong during light sensor init!"));
+    SensReady = false;
+  }
+  apds.setAmbientLightGain (GGAIN_4X);
+  delay(500);
+}
 void InitTCS34725 (){
-  // if (SENSSETT["GAIN"].as<int>() == 1)  {tcs.setGain (TCS34725_GAIN_1X);}
-  // if (SENSSETT["GAIN"].as<int>() == 4)  {tcs.setGain (TCS34725_GAIN_4X);}
-  // if (SENSSETT["GAIN"].as<int>() == 16) {tcs.setGain (TCS34725_GAIN_16X);}
-  // if (SENSSETT["GAIN"].as<int>() == 60) {tcs.setGain (TCS34725_GAIN_60X);}
-  // if (SENSSETT["INTEG"].as<float>() == 2.4) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_2_4MS);}
-  // if (SENSSETT["INTEG"].as<float>() == 24) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_24MS);}
-  // if (SENSSETT["INTEG"].as<float>() == 50) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_50MS);}
-  // if (SENSSETT["INTEG"].as<float>() == 101) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_101MS);}
-  // if (SENSSETT["INTEG"].as<float>() == 154) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_154MS);}
-  // if (SENSSETT["INTEG"].as<float>() == 700) {tcs.setIntegrationTime ( TCS34725_INTEGRATIONTIME_614MS);}
+  if (SENSSETT["GAIN"].as<int>() == 1)  {tcs.setGain (TCS34725_GAIN_1X);}
+  if (SENSSETT["GAIN"].as<int>() == 4)  {tcs.setGain (TCS34725_GAIN_4X);}
+  if (SENSSETT["GAIN"].as<int>() == 16) {tcs.setGain (TCS34725_GAIN_16X);}
+  if (SENSSETT["GAIN"].as<int>() == 60) {tcs.setGain (TCS34725_GAIN_60X);}
+  if (SENSSETT["INTEG"].as<float>() == 2.4) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_2_4MS);}
+  if (SENSSETT["INTEG"].as<float>() == 24) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_24MS);}
+  if (SENSSETT["INTEG"].as<float>() == 50) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_50MS);}
+  if (SENSSETT["INTEG"].as<float>() == 101) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_101MS);}
+  if (SENSSETT["INTEG"].as<float>() == 154) {tcs.setIntegrationTime (TCS34725_INTEGRATIONTIME_154MS);}
+  if (SENSSETT["INTEG"].as<float>() == 700) {tcs.setIntegrationTime ( TCS34725_INTEGRATIONTIME_614MS);}
   if (tcs.begin()) {
     Serial.println("Found TCS34725 sensor");
 
-    if (SENSSETT["LIGHT"] == false) {
+    if (SENSSETT["LIGHT"] == "False") {
       Serial.println("set LED off");
       digitalWrite(pinLED, LOW);
     } else {
@@ -417,19 +484,28 @@ void DefaultJsonBurn (){
   BURNSETT["ERROR"]["B"]=50;
 }
 void DefaultJsonMqtt (){
-  MQTTSETT["HOST"] = IPAddress (192,168,7,2);
+  MQTTSETT["HOST"] = "192.168.7.119";
   MQTTSETT["USER"] = "USER"; 
-  MQTTSETT["PASS"] = "PASS"; 
+  MQTTSETT["PASW"] = "PASS"; 
+  MQTTSETT["PORT"] = 1883; 
   MQTTSETT["HASS"] = true;
+  MQTTSETT["ENAB"] = false;
+  MQTTSETT["TOPC"] = "OilMeter";
 }
 void DefaultJsonSens (){
-  SENSSETT["INTEG"] = 50;
+  SENSSETT["TYPE"] = "TCS34725"; 
   SENSSETT["GAIN"] =  4; 
+  SENSSETT["INTEG"] = 50;
   SENSSETT["LIGHT"] = false; 
+}
+void DefaultJsonUsage (){
+  USAGE["LASTWAIT"] = 0;
+  USAGE["LASTCONS"] = 0;
+  USAGE["LASTTIME"] = 0;
 }
 
 void PublishHASS (){
-if (MQTTSETT["HASS"] == true){
+if (MQTTSETT["HASS"] == "True"){
   const char origArray[][2][20]= 
   {
       {"sensor/0/Color_R","Color_R"}, 
